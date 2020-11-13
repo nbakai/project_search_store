0ccc6c797d9c19563862d1e5cea042a620b906e2
Melisa Budini <melixbq@gmail.com>
2020-11-13 20:15:58 -0300

    user

--- config/routes.rb
+++ config/routes.rb
@@ -6,8 +6,11 @@ Rails.application.routes.draw do
   }
   devise_for :users, controllers: {
     registrations: 'users/registrations',
-    sessions: 'users/sessions'
+    sessions: 'users/sessions',
   }
+  devise_scope :user do
+    delete 'sign_out', to: 'devise/sessions#destroy'
+  end
   
   resources :categories
   root to: 'categories#index'
