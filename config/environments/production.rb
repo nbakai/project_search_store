c00a85b9592082fe87182ff2d9430737d5557567
Melisa Budini <melixbq@gmail.com>
2020-11-14 17:08:31 -0300

    devise email

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,13 +91,5 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
-#   config.action_mailer.smtp_settings = {
-#     address: "smtp.gmail.com",
-#     port: 587,
-#     domain: "gmail.com",
-#     authentication: "plain",
-#     enable_starttls_auto: true,
-#     user_name: ENV["GMAIL_USERNAME"],
-#     password: ENV["GMAIL_PASSWORD"]
-# }
+
 end
