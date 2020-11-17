a99616a6ab370c21f27aa24b5a117b33210aa25e
Melisa Budini <melixbq@gmail.com>
2020-11-17 01:03:56 -0300

    validate rati

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -5,4 +5,5 @@ class Comment < ApplicationRecord
   def blank_stars
     5 - rating.to_i
   end
+  validates :rating,  numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 0, only_integer: true }
 end
