cd29c5ce3890b36139477c715f4a19815a1cd6a4
Melisa Budini <melixbq@gmail.com>
2020-11-17 18:17:39 -0300

    user model

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,4 +7,13 @@ class User < ApplicationRecord
   has_one_attached :avatar, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :likes, dependent: :destroy
+
+  def self.from_omniauth(auth)
+    where(provider: auth.provider, uid: auth.uid).first_or_create do |user|
+      user.email = auth.info.email
+      user.password = Devise.friendly_token[0,20]
+      user.name = auth.info.name 
+    end
+  end
+  
 end
