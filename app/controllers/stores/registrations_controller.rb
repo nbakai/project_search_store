49f4a064c62102403e8ebe8dd63ce13f42237fe5
Melisa Budini <melixbq@gmail.com>
2020-11-18 14:15:24 -0300

    edit store

--- app/controllers/stores/registrations_controller.rb
+++ app/controllers/stores/registrations_controller.rb
@@ -51,9 +51,9 @@ class Stores::RegistrationsController < Devise::RegistrationsController
   end
 
   # The path used after sign up.
-  # def after_sign_up_path_for(resource)
-  #   super(resource)
-  # end
+  def after_sign_up_path_for(resource)
+    new_storextra_path(current_store)
+  end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
