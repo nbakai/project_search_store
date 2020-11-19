2e542eb16a1b77dbd8fced8055d7c944a69d9720
Melisa Budini <melixbq@gmail.com>
2020-11-19 01:23:06 -0300

    send

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -8,7 +8,7 @@ content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywh
 mail = SendGrid::Mail.new(from, subject, to, content)
 
 sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-response = sg.client.mail._('send').post(request_body: mail.to_json)
+response = sg.client.mail._('send').post(request_body: mail)
 puts response.status_code
 puts response.body
 puts response.parsed_body
