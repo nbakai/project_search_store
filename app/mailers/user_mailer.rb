94359b79b8e4867495d5a1d7d7e94262d0735631
Melisa Budini <melixbq@gmail.com>
2020-11-19 01:26:27 -0300

    send

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,15 +1,15 @@
-require 'sendgrid-ruby'
-include SendGrid
+# require 'sendgrid-ruby'
+# include SendGrid
 
-from = SendGrid::Email.new(email: 'melixbq@gmail.com')
-to = SendGrid::Email.new(email: 'melixbq@gmail.com')
-subject = 'Sending with Twilio SendGrid is Fun'
-content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
-mail = SendGrid::Mail.new(from, subject, to, content)
+# from = SendGrid::Email.new(email: 'melixbq@gmail.com')
+# to = SendGrid::Email.new(email: 'melixbq@gmail.com')
+# subject = 'Sending with Twilio SendGrid is Fun'
+# content = SendGrid::Content.new(type: 'text/plain', value: 'and easy to do anywhere, even with Ruby')
+# mail = SendGrid::Mail.new(from, subject, to, content)
 
-sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
-response = sg.client.mail._('send').post(request_body: mail)
-puts response.status_code
-puts response.body
-puts response.parsed_body
-puts response.headers
+# sg = SendGrid::API.new(api_key: ENV['SENDGRID_API_KEY'])
+# response = sg.client.mail._('send').post(request_body: mail.to_json)
+# puts response.status_code
+# puts response.body
+# puts response.parsed_body
+# puts response.headers
