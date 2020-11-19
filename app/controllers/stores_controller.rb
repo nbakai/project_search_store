84dd14add4c551adfdbea9d626ff68d6753fcdff
Melisa Budini <melixbq@gmail.com>
2020-11-18 23:07:28 -0300

    charts

--- app/controllers/stores_controller.rb
+++ app/controllers/stores_controller.rb
@@ -6,6 +6,9 @@ class StoresController < ApplicationController
     def show 
       @store  = Store.find(params[:id])
       @storextra = Storextra.find(@store.id)
+      
+      @chart_store = Product.where(store_id: current_store).group_by_day(:created_at).count
+
     end
   
 end
