820f09fdb05f06f9f4f7fe5fcf2b92c4e47e5092
Melisa Budini <melixbq@gmail.com>
2020-11-21 14:13:26 -0300

    cod orden

--- app/models/order.rb
+++ app/models/order.rb
@@ -1,7 +1,31 @@
 class Order < ApplicationRecord
+  before_create -> { generate_number(hash_size) }
   belongs_to :product
   belongs_to :user
   #belongs_to :store
   belongs_to :billing, optional: true
+
+  validates :number, uniqueness: true
+
+  validates :number, uniqueness: true
+
+  def generate_number(size)
+    self.number ||= loop do
+      random = random_candidate(size)
+      break random unless self.class.exists?(number: random)
+    end
+  end
+
+  def random_candidate(size)
+    "#{hash_prefix}#{Array.new(size){rand(size)}.join}"
+  end
+
+  def hash_prefix
+    "BO"
+  end
+
+  def hash_size
+    9
+  end
 
 end
