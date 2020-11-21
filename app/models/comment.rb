2176a3df06987f66aa0af6dd7a435d86ffdcc101
Melisa Budini <melixbq@gmail.com>
2020-11-21 17:53:32 -0300

    forms view

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -2,8 +2,12 @@ class Comment < ApplicationRecord
   belongs_to :user
   belongs_to :storextra
 
+  
+  validates :rating,  numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 0, only_integer: true }
+  validates :content, length: {maximum: 1000, message: 'El máximo es de 1000 caracteres!'}
+
   def blank_stars
     5 - rating.to_i
   end
-  validates :rating,  numericality: { less_than_or_equal_to: 5, greater_than_or_equal_to: 0, only_integer: true }
+
 end
