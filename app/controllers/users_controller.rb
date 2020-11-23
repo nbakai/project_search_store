a0f0228ae1a496bb15d3d21e19f6ccf9ac7d5c0d
Melisa Budini <melixbq@gmail.com>
2020-11-23 00:05:50 -0300

    user profile

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -7,15 +7,19 @@ class UsersController < ApplicationController
     @user  = User.find(params[:id])
     @order = Order.all 
     @product = Product.all
-    if @order.find_by(user_id: current_user)
-      @chart_user = Order.where(user_id: current_user).group_by_day(:created_at).count
-      @chart2_user = Comment.where(user_id: current_user).group_by_hour(:created_at).count
+    # if @order.find_by(user_id: current_user)
+    #   @chart_user = Order.where(user_id: current_user).group_by_day(:created_at).count
+    #   @chart2_user = Comment.where(user_id: current_user).group_by_hour(:created_at).count
+    # end
+    if @order.find_by(user_id: @user.id)
+      @chart_user = Order.where(user_id: @user.id).group_by_day(:created_at).count
+      @chart2_user = Comment.where(user_id: @user.id).group_by_hour(:created_at).count
     end
     # if @order.find_by(user_id: current_user) 
     #   @order_count = @order.find_by(user_id: current_user)
     #   @oder_count_sum = @order_count.sum
     # end
-    @order = current_order
+    # @order = current_order
 
     #@chart_visits = Visit.where(user: current_user).group_by_day(:started_at, last: 7).count
   end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,3 +1,4 @@
 class Category < ApplicationRecord
     has_many :storextras, dependent: :destroy
+
 end
