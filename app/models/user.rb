af84b903a8b97c7fe6886888cbbd25081f597236
Melisa Budini <melixbq@gmail.com>
2020-11-24 16:58:52 -0300

    fb up

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,7 +21,6 @@ class User < ApplicationRecord
       user.email = auth.info.email
       user.password = Devise.friendly_token[0,20]
       user.name = auth.info.name 
-      user.avatar = auth.info.image
     end
   end
 
