34c63de00820783f210adeec8c83f719f30957a9
Melisa Budini <melixbq@gmail.com>
2020-11-25 00:23:55 -0300

    registration

--- app/controllers/stores/registrations_controller.rb
+++ app/controllers/stores/registrations_controller.rb
@@ -51,11 +51,11 @@ class Stores::RegistrationsController < Devise::RegistrationsController
   end
 
   # The path used after sign up.
-  def after_sign_up_path_for(resource)
-    @store = current_store.id
-    @storextra = Storextra.new(id: @store)
-    new_storextra_path(current_store, @storextra)
-  end
+  # def after_sign_up_path_for(resource)
+  #   @store = current_store.id
+  #   @storextra = Storextra.new(id: @store)
+  #   new_storextra_path(current_store, @storextra)
+  # end
 
   # The path used after sign up for inactive accounts.
   # def after_inactive_sign_up_path_for(resource)
